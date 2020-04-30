678b77d64d0df96b73976c139183f2d213fe92ad
joedono <fjzusi@gmail.com>
2020-04-30 12:24:21 -0400

    Possibly fixed deployment

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
-var distDir = __dirname + "/dist/";
+var distDir = __dirname + "/dist";
 app.use(express.static(distDir));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
